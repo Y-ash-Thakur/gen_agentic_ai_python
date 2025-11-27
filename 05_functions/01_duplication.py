38ec27940b94829324579014bef514aec2ba6acb
Y-ash-Thakur <yasht0509@gmail.com>
2025-11-27 21:03:08 +0530

    function intro -> duplication and complex task

---
+++ 05_functions/01_duplication.py
@@ -0,0 +1,9 @@
+def print_order(name, chai_type):
+    print(f"{name}, ordered {chai_type} chai.")
+
+print_order("Lucy", "masala")
+print_order("Mark", "ginger")
+print_order("Sophia", "cardamom")
+print_order("James", "tulsi")
+print_order("Emma", "saffron")
+print_order("Liam", "black")
