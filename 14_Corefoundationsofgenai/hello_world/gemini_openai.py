3face30d5fca1d368635d0fb5b1d78a1c685c1ab
Y-ash-Thakur <yasht0509@gmail.com>
2026-04-30 13:37:25 +0530

    Gemini openai compatible api

---
+++ 14_Corefoundationsofgenai/hello_world/gemini_openai.py
@@ -0,0 +1,19 @@
+from openai import OpenAI
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+client = OpenAI(
+    api_key=os.getenv("GEMINI_API_KEY"),
+    base_url="https://generativelanguage.googleapis.com/v1beta/openai/"
+)
+
+response = client.chat.completions.create(
+    model="gemini-1.5-flash",
+    messages=[{
+        "role": "user", "content": "Hey There"
+    }]
+)
+
+print(response.choices[0].message.content)
